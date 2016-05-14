fae46e6356e5b9a9869296dfbd4079aba1f58597
Kostya <Kostya.Bakay@gmail.com>
2016-05-15 00:22:35 +0200

    Implemented saving changes of device name in SharedPreferences

--- app/src/main/java/com/example/fooandbar/ibeacon/MainActivity.java
+++ app/src/main/java/com/example/fooandbar/ibeacon/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.fooandbar.ibeacon.fragment.SettingsFragment;
 import com.example.fooandbar.ibeacon.fragment.UserDetailsFragment;
@@ -46,9 +48,6 @@ public class MainActivity extends AppCompatActivity implements ProximityManager.
         proximityManager = new KontaktProximityManager(this);
         setupPreferences(); //sets preferences
 
-        // For testing SettingsFragment
-        // addSettingsFragment();
-
         // For testing UserDetailsFragment
         // addUserDetailsFragment();
     }
@@ -72,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements ProximityManager.
     private void addSettingsFragment() {
         SettingsFragment fragment = new SettingsFragment();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.activity_main, fragment);
+        ft.replace(R.id.activity_main, fragment).addToBackStack(null);
         ft.commit();
     }
 
@@ -186,5 +185,30 @@ public class MainActivity extends AppCompatActivity implements ProximityManager.
     @Override
     public void onScanStop() {
         Log.d(TAG, "scanning is stopped");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(MainActivity.class.getSimpleName(), "onCreateOptionsMenu");
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(MainActivity.class.getSimpleName(), "onOptionsItemSelected");
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            addSettingsFragment();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
