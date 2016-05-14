77af9de13bbe02b5b208fe36e78330ce589705e3
viogull <viogull@gmail.com>
2016-05-14 21:19:53 +0300

    IBeacon Event Listener added v2

--- app/src/main/java/com/example/fooandbar/ibeacon/model/UserDevice.java
+++ app/src/main/java/com/example/fooandbar/ibeacon/model/UserDevice.java
@@ -4,4 +4,41 @@ package com.example.fooandbar.ibeacon.model;
  * Created by rsv on 14.05.2016.
  */
 public class UserDevice {
+    private String name;
+    private String idBeacon;
+    private String userID;
+    private double distance;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdBeacon() {
+        return idBeacon;
+    }
+
+    public void setIdBeacon(String idBeacon) {
+        this.idBeacon = idBeacon;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
 }
