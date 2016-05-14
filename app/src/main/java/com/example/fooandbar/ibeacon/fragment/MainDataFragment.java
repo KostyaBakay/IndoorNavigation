9d7eb51cd2c77bbd9b22d4a6baa4a9dea3758510
roma <xxRomanxx@ukr.net>
2016-05-14 16:21:28 +0300

    fragment folder added

---
+++ app/src/main/java/com/example/fooandbar/ibeacon/fragment/MainDataFragment.java
@@ -0,0 +1,17 @@
+package com.example.fooandbar.ibeacon.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fooandbar.ibeacon.R;
+
+public class MainDataFragment extends Fragment {
+
+    
+
+}
