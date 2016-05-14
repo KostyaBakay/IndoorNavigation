bbad83efd4380b481cca9e8837020c8d137ebb66
roma <xxRomanxx@ukr.net>
2016-05-14 16:32:19 +0300

    some folders added

--- app/src/main/java/com/example/fooandbar/ibeacon/fragment/MainDataFragment.java
+++ app/src/main/java/com/example/fooandbar/ibeacon/fragment/MainDataFragment.java
@@ -3,15 +3,25 @@ package com.example.fooandbar.ibeacon.fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.fooandbar.ibeacon.R;
 
 public class MainDataFragment extends Fragment {
+    ListView roomListView;
 
-    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_data,container);
+        roomListView = (ListView) view.findViewById(R.id.roomListView);
 
+
+
+        return  view;
+    }
 }
