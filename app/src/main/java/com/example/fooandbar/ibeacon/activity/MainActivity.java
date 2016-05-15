d1f680c47fb82e59792f6ecdf2111a4723a90a56
viogull <viogull@gmail.com>
2016-05-15 06:48:44 +0300

    Added BluetoothDialog

--- app/src/main/java/com/example/fooandbar/ibeacon/activity/MainActivity.java
+++ app/src/main/java/com/example/fooandbar/ibeacon/activity/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.fooandbar.ibeacon.activity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -20,11 +23,14 @@ import com.example.fooandbar.ibeacon.utils.PreferencesUtil;
 import com.kontakt.sdk.android.ble.configuration.scan.ScanContext;
 import com.kontakt.sdk.android.ble.manager.ProximityManagerContract;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 public class MainActivity extends AppCompatActivity  {
     private final String TAG = "MainActivity";
     private final String TAGNETW = "NetworkBeaconRequest";
     private ProximityManagerContract proximityManager;
     private ScanContext scanContext;
+    private SweetAlertDialog sweetAlertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +38,28 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
         setupUI();
         setupPreferences(); //sets preferences
-        startService(new Intent(MainActivity.this,BeaconListenerService.class));
-        addMainDataFragment();
+        sweetAlertDialog = new SweetAlertDialog(MainActivity.this,SweetAlertDialog.WARNING_TYPE);
+
+        if(isBluetoothAvailable()) {
+            startService(new Intent(MainActivity.this, BeaconListenerService.class));
+            addMainDataFragment();
+        }
+        else
+        {
+            sweetAlertDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+            sweetAlertDialog.setTitleText("You have turned off a Bluetooth");
+            sweetAlertDialog.setCancelable(false);
+            sweetAlertDialog.setConfirmText("Ok, i will open settings");
+            sweetAlertDialog.setContentText(" Please, " +
+                    "open settings and turn on Bluetooth");
+            sweetAlertDialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                @Override
+                public void onClick(SweetAlertDialog sweetAlertDialog) {
+                    startActivity(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
+                }
+            });
+            sweetAlertDialog.show();
+        }
     }
 
     public void addMainDataFragment() {
@@ -81,6 +107,11 @@ public class MainActivity extends AppCompatActivity  {
         if (!PreferencesUtil.canVerified(this)) {
             startActivity(new Intent(this, IntroActivity.class));
         }
+
+        if(isBluetoothAvailable())
+            sweetAlertDialog.dismiss();
+        else
+            sweetAlertDialog.show();
     }
 
     @Override
@@ -110,5 +141,10 @@ public class MainActivity extends AppCompatActivity  {
     private void setupUI() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    public static boolean isBluetoothAvailable() {
+        final BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        return (bluetoothAdapter != null && bluetoothAdapter.isEnabled());
     }
 }
